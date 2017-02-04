1c162bcf6dffbda44982fd40e3b915b45dd34d23
Administrator <602037504@qq.com>
2017-02-04 17:07:45 +0800

    完善项目的gradle文件

--- topbar/src/main/java/com/bigademo/topbar/DensityUtil.java
+++ topbar/src/main/java/com/bigademo/topbar/DensityUtil.java
@@ -11,25 +11,32 @@ public class DensityUtil {
 
     /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     * @param context
+     * @param dpValue
+     * @return
      */
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
 
+
     /**
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     * @param context
+     * @param pxValue
+     * @return
      */
     public static int px2dip(Context context, float pxValue) {
+
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
     /**
      * 将px值转换为sp值，保证文字大小不变
      *
+     * @param context
      * @param pxValue
-     * @param fontScale
-     *            （DisplayMetrics类中属性scaledDensity）
      * @return
      */
     public static int px2sp(Context context, float pxValue) {
@@ -40,9 +47,8 @@ public class DensityUtil {
     /**
      * 将sp值转换为px值，保证文字大小不变
      *
+     * @param context
      * @param spValue
-     * @param fontScale
-     *            （DisplayMetrics类中属性scaledDensity）
      * @return
      */
     public static int sp2px(Context context, float spValue) {
