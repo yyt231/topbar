de68118ea351c8436482d663b2f489c192dbacb7
Administrator <602037504@qq.com>
2017-02-07 10:03:41 +0800

    修改控件的类型为枚举类型&&修正文本的字号

--- topbar/README.md
+++ topbar/README.md
@@ -1,16 +1,135 @@
-   <com.bigademo.topbar.TopBar
+#更新内容
+- 更新control_type的类型为枚举类型
+- 修改control_textsize的大小，推荐使用SP
+#TopBar
+包含3个控件（左中右），其中中间为TextView，左右两个控件可以是ImageView或TextView
+##属性介绍
+#####topbar_left_control_type和topbar_right_control_type（必要属性）
+左右两个控件的类型，判断该控件为ImageView还是TextView。**这个属性使用imageview和textview两个枚举值**
+```
+<attr name="topbar_left_control_type">
+        <enum name="imageview" value="100" />
+        <enum name="textview" value="101" />
+</attr>
+<attr name="topbar_right_control_type" >
+    <enum name="imageview" value="200"/>
+    <enum name="textview" value="201"/>
+</attr>
+```
+#####topbar_left_control_src和topbar_right_control_src
+当topbar_left_control_type和topbar_right_control_type和topbar_left_control_type和topbar_right_control_type为imgview的时候，才能生效。
+```
+<attr name="topbar_left_control_src" format="reference" />
+<attr name="topbar_right_control_src" format="reference" />
+```
+#####topbar_left_control_text和topbar_right_control_textcolor
+和TextView的andorid:text一样
+```
+<attr name="topbar_left_control_text" format="string|reference" />
+<attr name="topbar_right_control_text" format="string|reference" />
+```
+#####topbar_left_control_textsize和topbar_right_control_textsize
+和TextVIew的textSize一样，**推荐使用sp的单位**
+```
+<attr name="topbar_left_control_textsize" format="dimension" />
+<attr name="topbar_right_control_textsize" format="dimension" />
+```
+最后列出所有属性
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <declare-styleable name="topbar_layout">
+        <!--左面控件的类型-->
+        <attr name="topbar_left_control_type">
+            <enum name="imageview" value="100" />
+            <enum name="textview" value="101" />
+        </attr>
+        <attr name="topbar_left_control_widht" format="dimension" />
+        <attr name="topbar_left_control_height" format="dimension" />
+
+        <!--左面的控件为文字-->
+        <attr name="topbar_left_control_text" format="string|reference" />
+        <!--左面的控件的背景颜色-->
+        <attr name="topbar_left_control_bgcolor" format="color|reference" />
+        <!--左面的控件的文字大小-->
+        <attr name="topbar_left_control_textsize" format="dimension" />
+        <!--左面的控件的文字颜色-->
+        <attr name="topbar_left_control_textcolor" format="color|reference" />
+        <!--end 左面的控件为文字-->
+
+        <!--左面的控件为ImageView-->
+        <!--左间控件的src-->
+        <attr name="topbar_left_control_src" format="reference" />
+        <!--end 左面的控件为ImageView-->
+
+        <!--右面控件的类型-->
+        <attr name="topbar_right_control_type" >
+            <enum name="imageview" value="200"/>
+            <enum name="textview" value="201"/>
+        </attr>
+        <attr name="topbar_right_control_widht" format="dimension" />
+        <attr name="topbar_right_control_height" format="dimension" />
+
+        <!--右的控件为文字-->
+        <!--右控件的文字-->
+        <attr name="topbar_right_control_text" format="string|reference" />
+        <!--右控件的背景颜色-->
+        <attr name="topbar_right_control_bgcolor" format="color|reference" />
+        <!--右控件的文字大小-->
+        <attr name="topbar_right_control_textsize" format="dimension" />
+        <!--右控件的文字颜色-->
+        <attr name="topbar_right_control_textcolor" format="color|reference" />
+        <!--end 左面的控件为文字-->
+        <!--右的控件为ImageView-->
+        <!--右控件的src-->
+        <attr name="topbar_right_control_src" format="reference" />
+        <!--end 左面的控件为ImageView-->
+
+
+        <!--中间控件的文字-->
+        <attr name="topbar_middle_control_text" format="string|reference" />
+        <!--中间控件的背景颜色-->
+        <attr name="topbar_middle_control_bgcolor" format="color|reference" />
+        <!--中间控件的文字大小-->
+        <attr name="topbar_middle_control_textsize" format="dimension" />
+        <!--中间控件的文字颜色-->
+        <attr name="topbar_middle_control_textcolor" format="color|reference" />
+
+    </declare-styleable>
+</resources>
+```
+##举个例子
+```
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    xmlns:topbar="http://schemas.android.com/apk/res-auto"
+    android:id="@+id/activity_main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:paddingBottom="@dimen/activity_vertical_margin"
+    tools:context="com.tony.topdemo.MainActivity">
+
+
+    <com.bigademo.topbar.TopBar
         android:id="@+id/topbar"
         android:layout_width="match_parent"
-        android:layout_height="56dp"
-        android:background="#00fff0"
-        topbar:left_control_src="@drawable/back"
-        topbar:left_control_type="ImageView"
-        topbar:middle_control_text="我的棋室"
-        topbar:middle_control_textcolor="@android:color/white"
-        topbar:middle_control_textsize="22px"
-        topbar:right_control_text="创建棋室"
-        topbar:right_control_type="TextView" />
+        android:layout_height="48dp"
+        android:background="@android:color/holo_blue_bright"
+        topbar:topbar_left_control_text="11111111"
+        topbar:topbar_left_control_textcolor="@android:color/black"
+        topbar:topbar_left_control_textsize="22sp"
+        topbar:topbar_left_control_type="textview"
+
+        topbar:topbar_middle_control_text="11111"
+        topbar:topbar_middle_control_textsize="22sp"
+        topbar:topbar_right_control_src="@mipmap/ic_launcher"
+        topbar:topbar_right_control_text="22222"
+        topbar:topbar_right_control_textsize="22sp"
+        topbar:topbar_right_control_type="textview" />
 
 
-left_control_type和right_control_type 必须设置这一项为 ImageView 或 TextView
-Middle Textsize 只能为px。
+</LinearLayout>
+
+```
--- topbar/src/main/java/com/bigademo/topbar/TopBar.java
+++ topbar/src/main/java/com/bigademo/topbar/TopBar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,39 +47,53 @@ public class TopBar extends RelativeLayout {
         super(context, attrs);
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.topbar_layout);
-        String leftControlType = ta.getString(R.styleable.topbar_layout_left_control_type);
+//        String leftControlType = ta.getString(R.styleable.topbar_layout_topbar_left_control_type);
+        long leftControlType = ta.getInt(R.styleable.topbar_layout_topbar_left_control_type,0);
+       if(leftControlType==100){
+           leftImgBtn = new ImageView(context);
+           leftImgBtn.setImageDrawable(ta.getDrawable(R.styleable.topbar_layout_topbar_left_control_src));
+       }else if(leftControlType==101){
+           leftBtn = new TextView(context);
+           leftBtn.setText(ta.getString(R.styleable.topbar_layout_topbar_left_control_text));
+           leftBtn.setTextColor(ta.getInt(R.styleable.topbar_layout_topbar_left_control_textcolor, View.NO_ID));
+           float textSize=ta.getDimensionPixelSize(R.styleable.topbar_layout_topbar_left_control_textsize,defTextSize);//获取的值的单位为SP
+           //转为PX格式
+           leftBtn.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
+       }
 
-        if ("ImageView".equalsIgnoreCase(leftControlType)) {
-            leftImgBtn = new ImageView(context);
-            leftImgBtn.setImageDrawable(ta.getDrawable(R.styleable.topbar_layout_left_control_src));
-        } else if ("TextView".equalsIgnoreCase(leftControlType)) {
-            leftBtn = new TextView(context);
-            leftBtn.setText(ta.getString(R.styleable.topbar_layout_left_control_text));
-            leftBtn.setTextColor(ta.getInt(R.styleable.topbar_layout_left_control_textcolor, View.NO_ID));
-            leftBtn.setTextSize(ta.getDimensionPixelSize(R.styleable.topbar_layout_left_control_textsize, defTextSize));
-        } else {
-        }
 
 
-        String rightControlType = ta.getString(R.styleable.topbar_layout_right_control_type);
-        if ("ImageView".equalsIgnoreCase(rightControlType)) {
+//        String rightControlType = ta.getString(R.styleable.topbar_layout_topbar_right_control_type);
+        int rightControlType = ta.getInt(R.styleable.topbar_layout_topbar_right_control_type,0);
+        if(rightControlType==200){
             rightImgBtn = new ImageView(context);
-            rightImgBtn.setImageDrawable(ta.getDrawable(R.styleable.topbar_layout_right_control_src));
-        } else if ("TextView".equalsIgnoreCase(rightControlType)) {
+            rightImgBtn.setImageDrawable(ta.getDrawable(R.styleable.topbar_layout_topbar_right_control_src));
+        }else if(rightControlType==201){
             rightBtn = new TextView(context);
-            rightBtn.setText(ta.getString(R.styleable.topbar_layout_right_control_text));
-            rightBtn.setTextColor(ta.getInt(R.styleable.topbar_layout_right_control_textcolor, View.NO_ID));
-            rightBtn.setTextSize(ta.getDimensionPixelSize(R.styleable.topbar_layout_right_control_textsize, defTextSize));
-        } else {
+            rightBtn.setText(ta.getString(R.styleable.topbar_layout_topbar_right_control_text));
+            rightBtn.setTextColor(ta.getInt(R.styleable.topbar_layout_topbar_right_control_textcolor, View.NO_ID));
+            float textSize=ta.getDimensionPixelSize(R.styleable.topbar_layout_topbar_right_control_textsize,defTextSize);
+            rightBtn.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
         }
+//        if ("ImageView".equalsIgnoreCase(rightControlType)) {
+//            rightImgBtn = new ImageView(context);
+//            rightImgBtn.setImageDrawable(ta.getDrawable(R.styleable.topbar_layout_topbar_right_control_src));
+//        } else if ("TextView".equalsIgnoreCase(rightControlType)) {
+//            rightBtn = new TextView(context);
+//            rightBtn.setText(ta.getString(R.styleable.topbar_layout_topbar_right_control_text));
+//            rightBtn.setTextColor(ta.getInt(R.styleable.topbar_layout_topbar_right_control_textcolor, View.NO_ID));
+//            float textSize=ta.getDimensionPixelSize(R.styleable.topbar_layout_topbar_right_control_textsize,defTextSize);
+//            rightBtn.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
+//        } else {
+//        }
 
 
-        String middleText = ta.getString(R.styleable.topbar_layout_middle_control_text);
-        float textSize = ta.getDimension(R.styleable.topbar_layout_middle_control_textsize, defTextSize);
-        int textColor = ta.getColor(R.styleable.topbar_layout_middle_control_textcolor, context.getResources().getColor(android.R.color.black));
+        String middleText = ta.getString(R.styleable.topbar_layout_topbar_middle_control_text);
+        float textSize = ta.getDimensionPixelSize(R.styleable.topbar_layout_topbar_middle_control_textsize, defTextSize);
+        int textColor = ta.getColor(R.styleable.topbar_layout_topbar_middle_control_textcolor, context.getResources().getColor(android.R.color.black));
         middleTextView = new TextView(context);
         middleTextView.setText(middleText);
-        middleTextView.setTextSize(textSize);
+        middleTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
         middleTextView.setTextColor(textColor);
         ta.recycle();
 
@@ -87,7 +102,8 @@ public class TopBar extends RelativeLayout {
         leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
         leftParams.addRule(Gravity.CENTER);
 
-        if ("ImageView".equalsIgnoreCase(leftControlType)) {
+
+        if(leftControlType==100){
             leftParams = new LayoutParams(DensityUtil.dip2px(context, 48), LayoutParams.MATCH_PARENT);
             leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
             leftParams.addRule(Gravity.CENTER);
@@ -106,23 +122,31 @@ public class TopBar extends RelativeLayout {
                 }
             });
             addView(leftImgBtn, leftParams);
-        } else if ("TextView".equalsIgnoreCase(leftControlType)) {
+        }else if(leftControlType==101){
+            leftParams=new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
+            leftParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            leftParams.addRule(Gravity.CENTER);
+            leftParams.setMargins(DensityUtil.dip2px(context,16),0,0,0);
             leftBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     lisnter.leftClickLisnter();
                 }
             });
-
+            leftParams.addRule(Gravity.CENTER);
             addView(leftBtn, leftParams);
         }
+
+
+
 
         rightParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
         rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
         rightParams.addRule(Gravity.CENTER);
         rightParams.addRule(RelativeLayout.CENTER_VERTICAL);
         //判断右边的控件是TextView还是ImageView
-        if ("ImageView".equalsIgnoreCase(rightControlType)) {
+        if(rightControlType==200){
             rightParams = new LayoutParams(DensityUtil.dip2px(context, 48), LayoutParams.MATCH_PARENT);
             rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
             rightParams.addRule(Gravity.CENTER);
@@ -143,7 +167,7 @@ public class TopBar extends RelativeLayout {
             });
 
             addView(rightImgBtn, rightParams);
-        } else if ("TextView".equalsIgnoreCase(rightControlType)) {
+        }else if(rightControlType==201){
             rightParams = new LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
             rightParams.addRule(RelativeLayout.CENTER_VERTICAL);
@@ -159,6 +183,45 @@ public class TopBar extends RelativeLayout {
 
             addView(rightBtn, rightParams);
         }
+
+
+//        if ("ImageView".equalsIgnoreCase(rightControlType)) {
+//            rightParams = new LayoutParams(DensityUtil.dip2px(context, 48), LayoutParams.MATCH_PARENT);
+//            rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
+//            rightParams.addRule(Gravity.CENTER);
+//
+////            rightImgBtn.setPadding(
+////                    DensityUtil.dip2px(context,8),
+////                    DensityUtil.dip2px(context,8),
+////                    DensityUtil.dip2px(context,8),
+////                    DensityUtil.dip2px(context,8)
+////            );
+//            rightImgBtn.setScaleType(ImageView.ScaleType.CENTER);
+//
+//            rightImgBtn.setOnClickListener(new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    lisnter.rightClickListener();
+//                }
+//            });
+//
+//            addView(rightImgBtn, rightParams);
+//        } else if ("TextView".equalsIgnoreCase(rightControlType)) {
+//            rightParams = new LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+//            rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
+//            rightParams.addRule(RelativeLayout.CENTER_VERTICAL);
+//            rightParams.addRule(Gravity.CENTER);
+//            rightParams.setMargins(0,0,DensityUtil.dip2px(context,16),0);
+//            rightBtn.setOnClickListener(new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    lisnter.rightClickListener();
+//                }
+//            });
+//            rightParams.addRule(Gravity.CENTER);
+//
+//            addView(rightBtn, rightParams);
+//        }
 
 
         middleParms = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
